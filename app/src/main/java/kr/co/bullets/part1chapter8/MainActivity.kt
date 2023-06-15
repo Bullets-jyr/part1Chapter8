ca78181f00e8af826dcd1d5ec1031c6bdd9c8ca1
Bullets-jyr <jyr@bullets.co.kr>
2023-06-15 17:09:28 +0900

    01. 개요 및 학습 목표 && 02. 권한 가져오기

--- app/src/main/java/kr/co/bullets/part1chapter8/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part1chapter8/MainActivity.kt
@@ -1,11 +1,70 @@
 package kr.co.bullets.part1chapter8
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kr.co.bullets.part1chapter8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loadImageButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+            shouldShowRequestPermissionRationale(
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) -> {
+                showPermissionInfoDialog()
+            }
+            else -> {
+                requestReadExternalStorage()
+            }
+        }
+
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해서 외부 저장소 읽기 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의") { _, _ ->
+                requestReadExternalStorage()
+            }
+        }.show()
+    }
+
+    private fun loadImage() {
+        Toast.makeText(this, "이미지를 가져올 예정입니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun requestReadExternalStorage() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
+            REQUEST_READ_MEDIA_IMAGES
+        )
+    }
+
+    companion object {
+        const val REQUEST_READ_MEDIA_IMAGES = 100
     }
 }
