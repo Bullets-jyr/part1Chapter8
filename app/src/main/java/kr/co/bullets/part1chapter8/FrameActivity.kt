7b7163f8bc05f41c3ec135769a4e5d396776dab2
Bullets-jyr <jyr@bullets.co.kr>
2023-06-17 11:56:41 +0900

    07. 액자 UI 그리기 (4)

--- app/src/main/java/kr/co/bullets/part1chapter8/FrameActivity.kt
+++ app/src/main/java/kr/co/bullets/part1chapter8/FrameActivity.kt
@@ -3,6 +3,7 @@ package kr.co.bullets.part1chapter8
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.tabs.TabLayoutMediator
 import kr.co.bullets.part1chapter8.databinding.ActivityFrameBinding
 
@@ -13,6 +14,12 @@ class FrameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFrameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.apply {
+            title = "나만의 앨범"
+            setSupportActionBar(this)
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val images = (intent.getStringArrayExtra("images") ?: emptyArray()).map { uriString -> FrameItem(Uri.parse(uriString)) }
 
@@ -28,5 +35,15 @@ class FrameActivity : AppCompatActivity() {
             tab, position ->
             binding.viewPager.currentItem = tab.position
         }.attach()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/kr/co/bullets/part1chapter8/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part1chapter8/MainActivity.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
@@ -29,6 +31,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.toolbar.apply {
+            title = "사진 가져오기"
+            setSupportActionBar(this)
+        }
+
         binding.loadImageButton.setOnClickListener {
             checkPermission()
         }
@@ -37,6 +44,23 @@ class MainActivity : AppCompatActivity() {
             navigateToFrameActivity()
         }
         initRecyclerView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+                checkPermission()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     private fun navigateToFrameActivity() {
